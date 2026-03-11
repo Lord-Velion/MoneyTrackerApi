fe61530544cdd317d343a331a20c2c0769e04726
Lord-Velion <61228826+Lord-Velion@users.noreply.github.com>
2026-03-11 15:13:02 +0500

    Add stub accounts endpoints handler

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyTrackerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllAccounts()
+        {
+            return Ok(new {message = "Get all accounts"});
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAccountById(int id)
+        {
+            return Ok(new { message = "Get account by id" });
+        }
+
+        [HttpPost]
+        public IActionResult CreateAccount()
+        {
+            return Ok(new { message = "Create new account" });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateAccount()
+        {
+            return Ok(new { message = "Update account" });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAccount()
+        {
+            return Ok(new { message = "Delete account" });
+        }
+
+    }
+}
