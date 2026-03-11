bd5fecbe62bf88794d3056eacf65995ce6880436
Lord-Velion <61228826+Lord-Velion@users.noreply.github.com>
2026-03-11 15:23:22 +0500

    Add stub transactions endpoints handler

---
+++ Controllers/TransactionsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyTrackerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllTransactions()
+        {
+            return Ok(new { message = "Get all transactions" });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTransactionById(int id)
+        {
+            return Ok(new { message = "Get transaction by id" });
+        }
+
+        [HttpPost]
+        public IActionResult CreateTransaction()
+        {
+            return Ok(new { message = "Create new transaction" });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateTransaction()
+        {
+            return Ok(new { message = "Update transaction" });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTransaction()
+        {
+            return Ok(new { message = "Delete transaction" });
+        }
+    }
+}
