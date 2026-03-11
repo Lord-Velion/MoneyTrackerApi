14c1d2e52a370ddbc911156937be5483a9c33c3e
Lord-Velion <61228826+Lord-Velion@users.noreply.github.com>
2026-03-11 16:35:03 +0500

    Scaffold database context

---
+++ Models/MoneyTrackerDbContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MoneyTrackerApi.Models;
+
+public partial class MoneyTrackerDbContext : DbContext
+{
+    public MoneyTrackerDbContext()
+    {
+    }
+
+    public MoneyTrackerDbContext(DbContextOptions<MoneyTrackerDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Account> Accounts { get; set; }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Transaction> Transactions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-L48S00P;Database=MoneyTrackerDB;Trusted_Connection=true;TrustServerCertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Account>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Accounts__3214EC074FD7F775");
+
+            entity.HasIndex(e => e.Name, "UQ__Accounts__737584F62D4E73CF").IsUnique();
+
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Categori__3214EC07A2723E83");
+
+            entity.HasIndex(e => e.Name, "UQ__Categori__737584F68A307294").IsUnique();
+
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Transaction>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Transact__3214EC0754E7B51C");
+
+            entity.Property(e => e.Amount).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.Date)
+                .HasPrecision(0)
+                .HasDefaultValueSql("(getdate())");
+
+            entity.HasOne(d => d.Account).WithMany(p => p.Transactions)
+                .HasForeignKey(d => d.AccountId)
+                .HasConstraintName("FK_Transactions_Accounts");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Transactions)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK_Transactions_Categories");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoneyTrackerApi.Models;
+
+public partial class Transaction
+{
+    public int Id { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateTime? Date { get; set; }
+
+    public bool IsIncome { get; set; }
+
+    public int AccountId { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public virtual Account Account { get; set; } = null!;
+
+    public virtual Category Category { get; set; } = null!;
+}
