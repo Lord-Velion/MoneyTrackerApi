cfd1f5c30ca2ea9293e20cbf48f265faf44c80bb
Lord-Velion <61228826+Lord-Velion@users.noreply.github.com>
2026-03-11 15:19:34 +0500

    Add stub categories endpoints handler

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyTrackerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllCategories()
+        {
+            return Ok(new { message = "Get all categories" });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCategoryById(int id)
+        {
+            return Ok(new { message = "Get category by id" });
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory()
+        {
+            return Ok(new { message = "Create new category" });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateCategory()
+        {
+            return Ok(new { message = "Update category" });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCategory()
+        {
+            return Ok(new { message = "Delete category" });
+        }
+    }
+}
